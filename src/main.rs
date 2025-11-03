ebad8b96edcdc2251acf56eed4ef453e138479dc
Benjamin Massey <benjamin.w.massey@gmail.com>
2025-11-02 18:39:23 -0800

    first pass match filling, data structure reworks, some display tech

--- src/main.rs
+++ src/main.rs
@@ -7,38 +7,46 @@ fn main() {
     // init
     let mut ids: HashSet<u32> = HashSet::new();
     let mut bracket = Bracket {
-        players: vec![],
-        matches: vec![],
+        players: HashMap::new(),
+        matches: HashMap::new(),
     };
 
     //players
     let player_names = get_players(); // TODO: input
     for name in player_names {
-        bracket.players.push(Player {
-            id: PlayerId(new_id(&mut ids)),
-            name,
-        });
+        let id = PlayerId(new_id(&mut ids));
+        bracket.players.insert(id, Player { id, name });
     }
 
     // matches
-    for group in bracket.players.chunks(desired_size) {
-        let player_ids: Vec<PlayerId> = group.iter().map(|p| p.id).collect();
-        let player_results: HashMap<PlayerId, PlayerResult> = player_ids
+    for group in bracket
+        .players
+        .keys()
+        .map(|k| *k)
+        .collect::<Vec<PlayerId>>()
+        .chunks(desired_size)
+    {
+        let player_results: HashMap<PlayerId, PlayerResult> = group
             .iter()
             .map(|p| (*p, PlayerResult::Unplayed))
             .collect::<Vec<(PlayerId, PlayerResult)>>()
             .into_iter()
             .collect();
-        bracket.matches.push(Match {
-            id: MatchId(new_id(&mut ids)),
-            resulting_match: None, // TODO: does this make sense?
-            players: player_ids,
-            states: player_results,
-        });
+        let id = MatchId(new_id(&mut ids));
+        bracket.matches.insert(
+            id,
+            Match {
+                id,
+                resulting_match: None, // TODO: does this make sense?
+                players: group.to_vec(),
+                states: player_results,
+            },
+        );
     }
+    // TODO: some filtering process to make match counts optimal
 
     // display
-    // TODO:
+    bracket.display();
 }
 
 fn new_id(ids: &mut HashSet<u32>) -> u32 {
@@ -55,8 +63,19 @@ fn new_id(ids: &mut HashSet<u32>) -> u32 {
 }
 
 struct Bracket {
-    players: Vec<Player>,
-    matches: Vec<Match>,
+    players: HashMap<PlayerId, Player>,
+    matches: HashMap<MatchId, Match>,
+}
+impl Bracket {
+    fn display(&self) {
+        for (mid, m) in &self.matches {
+            println!("==============");
+            for p in &m.players {
+                println!("{} [{}]", self.players[p].name, m.states[p]);
+            }
+            println!("==============\n");
+        }
+    }
 }
 
 #[derive(Copy, Clone, Eq, PartialEq, Hash)]
@@ -71,16 +90,27 @@ enum PlayerResult {
     Pass,
     Fail,
 }
+impl std::fmt::Display for PlayerResult {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let s = match self {
+            PlayerResult::Unplayed => "U",
+            PlayerResult::Pass => "P",
+            PlayerResult::Fail => "F",
+        };
+        write!(f, "{}", s)
+    }
+}
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Eq, PartialEq, Hash)]
 struct MatchId(u32);
 struct Match {
     id: MatchId,
-    resulting_match: Option<MatchId>, // None = final
+    resulting_match: Option<MatchId>,
     players: Vec<PlayerId>,
     states: HashMap<PlayerId, PlayerResult>,
 }
 
+// TODO: real input system
 fn get_players() -> Vec<String> {
     vec![
         "Thomas".to_owned(),
